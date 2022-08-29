c288155df822a099952820de799a798deb3b935e
mondemonde <monde.justflowers@gmail.com>
2022-08-29 12:45:49 +0800

    init sakai

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component, VERSION } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+
+    menuMode = 'static';
+
+    constructor(private primengConfig: PrimeNGConfig) { }
+
+    ngOnInit() {
+        this.primengConfig.ripple = true;
+        document.documentElement.style.fontSize = '14px';
+    }
 }
